a613effc05b38ee9ef54118b4fd0cbfdd201048f
Gabor Szeles <gabor@szelesek.hu>
2022-09-29 16:20:22 +0100

    Added DB migration

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,7 +10,6 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
-
 class Command(BaseCommand):
     """Django command to wait for database."""
 
@@ -25,5 +24,5 @@ class Command(BaseCommand):
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
